cdcd9eb3f606b70f958daef88ff00e1d70d0a630
Wes <wrharrison23@gmail.com>
2021-01-11 11:01:27 -0500

    added array for fish objects in FishDataProvider

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,44 @@
+const fishCollection = [
+    {
+        type: "Beta Fish",
+        length: "2 inches",
+        name: "George",
+        location: "Galapagos Island",
+        food: "Pretzels"
+    },
+    {
+        type: "",
+        length: "",
+        name: "",
+        location: "",
+        food: ""
+    },
+    {
+        type: "",
+        length: "",
+        name: "",
+        location: "",
+        food: ""
+    },
+    {
+        type: "",
+        length: "",
+        name: "",
+        location: "",
+        food: ""
+    },
+    {
+        type: "",
+        length: "",
+        name: "",
+        location: "",
+        food: ""
+    },
+    {
+        type: "",
+        length: "",
+        name: "",
+        location: "",
+        food: ""
+    }
+]
