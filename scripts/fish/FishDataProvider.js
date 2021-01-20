e3f04afe8fabd6d75e56725c94175e60475074c2
Wes <wrharrison23@gmail.com>
2021-01-20 09:39:52 -0500

    added fish filter

--- scripts/fish/FishDataProvider.js
+++ scripts/fish/FishDataProvider.js
@@ -1,42 +1,42 @@
 const fishCollection = [
     {
         type: "Beta Fish",
-        length: "2 inches",
+        length: "7",
         name: "George",
         location: "Galapagos Island",
         food: "Pretzels"
     },
     {
         type: "Guppy",
-        length: "3 cm",
+        length: "35",
         name: "Phil",
         location: "Missouri",
         food: "Crackers"
     },
     {
         type: "Goldfish",
-        length: "1 inch",
+        length: "15",
         name: "Goldie",
         location: "China",
         food: "Pizza"
     },
     {
         type: "Bluefish",
-        length: "6 inches",
+        length: "6",
         name: "Bill",
         location: "Ohio",
         food: "Chicken"
     },
     {
         type: "Sunfish",
-        length: "2 feet",
+        length: "22",
         name: "Sunny",
         location: "Miami",
         food: "noodles"
     },
     {
         type: "Pike",
-        length: "1.5 feet",
+        length: "18",
         name: "Phil",
         location: "West Virginia",
         food: "grits"
@@ -47,3 +47,38 @@ const fishCollection = [
 export const useFish = () => {
     return fishCollection.slice()
 }
+
+export const mostHolyFish = () => {
+    // 3, 6, 9, 12, etc... fish
+    const holyFish = []
+
+    for (let fish of fishCollection) {
+        if (fish.length % 3 === 0)
+        holyFish.push(fish)
+    }
+
+    return holyFish
+}
+
+export const soldierFish = () => {
+    // 5, 10, 15, 20, 25, etc... fish
+   const soldierFish = []
+   for (let fish of fishCollection){
+    if (fish.length % 5 === 0)
+    soldierFish.push(fish)
+   }
+    
+    return soldierFish
+}
+
+export const nonHolyFish = () => {
+    // Any fish not a multiple of 3 or 5
+    const regularFish= []
+    for (let fish of fishCollection){
+        if (fish.length % 5 != 0 && fish.length % 3 != 0)
+        regularFish.push(fish)
+    }
+    return regularFish
+}
+
+console.log(nonHolyFish(fishCollection))
